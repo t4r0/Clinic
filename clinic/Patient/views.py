b850e9e37af2ed37cf2cf37b52bc0952b88d8ad4
krnmt <karenmaytz@hotmail.com>
2014-07-25 13:30:12 -0600

    Modelos y Vistas

--- clinic/Patient/views.py
+++ clinic/Patient/views.py
@@ -1,1 +1,19 @@
 # Create your views here.
+
+from django.http import Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+def viewProfile(request,cui):
+    user = User()
+    try:
+        user = User.objects.get(cui=cui)
+        if user == request.user:
+            return HttpResponseRedirect('/patient/'+cui)
+        except User.DoesNotExist:
+            raise Http404
+        return render(request)
+
+
+        
+
