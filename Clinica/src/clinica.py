e38239b502347dbc295df40eeecef6fce79d0df9
krnmt <karenmaytz@hotmail.com>
2014-07-25 10:19:22 -0600

    Creacion de proyecto

---
+++ Clinica/src/clinica.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="KarenM"
+__date__ ="$25-jul-2014 10:18:47$"
+
+if __name__ == "__main__":
+    print "Hello World"
