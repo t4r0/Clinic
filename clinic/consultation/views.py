37ff86b1aa76ade6d9347016ce28a1fa0fb73125
[JG - George] <j.or.alq@gmail.com>
2014-07-27 15:54:23 -0600

    Recurso añadido
    Se ha agregado vista para consulta.

--- clinic/consultation/views.py
+++ clinic/consultation/views.py
@@ -1,1 +1,16 @@
 # Create your views here.
+
+from django.shortcuts import render, render_to_response, Http404
+from django.templete import RequestContext
+from django.contrib.auth.decorators import login_requiered
+from django.contrib.auth.models import User
+
+def viewConsultation(request, DPI):
+	patient = Patient()
+	try:
+		patient = Patient.objects.get(DPI = DPI)
+		if patient == resquest.patient:
+			return render_to_response('/patient/consultation.html')
+	except Patiente.DoesNotExist:
+		raise Https404	
+	return render (request)
