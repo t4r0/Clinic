7c90e584c5128a8fe10714f5876e06a2b0bd1f59
Estuardo Díaz <esjoando555@gmail.com>
2014-07-28 01:27:40 -0600

    CORRECCION
    Se ha arreglado un conflicto de configuración

--- clinic/consultation/urls.py
+++ clinic/consultation/urls.py
@@ -1,6 +1,6 @@
 # Urls para el manejo de consultation
 
-from django.conf.urls import patterns, include, urls
+from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('', 
 	url(r'^consulta/', 'consultation.views.consultation_view', name='consulta'),
